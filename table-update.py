4f06705545c469bc668461be4e9a5209c2f4cff2
Rainier Schlekewey <rschlekewey@gmail.com>
2023-01-13 15:23:34 -0800

    updated so not reliant on sqlalchemy

--- table-update.py
+++ table-update.py
@@ -1,61 +1,51 @@
-import pandas as pd
 import snowflake.connector
-from snowflake.connector.pandas_tools import pd_writer
-from sqlalchemy import create_engine
-from snowflake.sqlalchemy import URL
 
-user = ''
-password = ''
-account = ''
-warehouse = ''
-database = ''
-schema = ''
-role = ''
+# Declare the Snowflake connection details as universal variables
+username = 'your_username'
+password = 'your_password'
+account = 'your_account'
 
-desired_table_name = ''
+def push_snowflake(dataframe, desired_table_name, warehouse, role, database, schema):
+    # Connect to Snowflake
+    con = snowflake.connector.connect(
+        user=username,
+        password=password,
+        account=account
+    )
 
-conn = snowflake.connector.connect(
-    user = user,
-    password = password,
-    account =  account,
-    warehouse = warehouse,
-    database = database,
-    schema = schema
-)
+    # Create a cursor
+    cur = con.cursor()
 
-cur = conn.cursor()
+    # Snowflake table
+    table_name = desired_table_name
+    table = '.'.join([database, schema, table_name])
 
-########## ADD CODE TO BUILD TABLE BELOW THIS COMMENT ##########
+    # check for unsupported data types
+    unsupported_types = set(dataframe.dtypes.astype(str).unique()).difference(set(['int','float','bool','object','datetime64[ns]','float64','int64','datetime64[ns, UTC]']))
+    if unsupported_types:
+        raise ValueError(f"Unsupported datatype found: {unsupported_types}. Please make sure that all datatype can be mapped to a Snowflake data type.")
+    # Check if table exists
+    table_exists = cur.execute("SHOW TABLES LIKE '{}'".format(table))
+    if not table_exists:
+        # Create the table with the proper columns and data types
+        columns = ''
+        for col, dtype in zip(dataframe.columns, dataframe.dtypes):
+            columns += col + ' ' + str(dtype) + ','
+        columns = columns[:-1] # remove the last comma
+        cur.execute("CREATE TABLE {}.{} ({})".format(schema, table_name, columns))
+        con.commit()
 
-sql = ''
-cur.execute(sql)
-df = cur.fetch_pandas_all()
+    cur.execute("USE WAREHOUSE {}".format(warehouse))
+    cur.execute("USE SCHEMA {}".format(schema))
+    cur.execute("USE DATABASE {}".format(database))
+    cur.execute("USE ROLE {}".format(role))
 
+    # Insert data into the table
+    data = [tuple(x) for x in dataframe.values]
+    placeholders = ','.join(['%s'] * len(dataframe.columns))
+    cur.executemany("INSERT INTO {} SELECT {}".format(table, placeholders), data)
+    con.commit()
 
-
-
-
-
-
-
-'''Function to push dataframe into snowflake, appends if table exists
-@param dataframe is the pandas dataframe that will be appended to the snowflake table
-@param snowflake_table_name is the name of the table in snowflake
-'''
-def push_snowflake(dataframe, snowflake_table_name):
-    engine = create_engine(URL(
-        account = account,
-        user = user,
-        password = password,
-        database = database,
-        schema = schema,
-        warehouse = warehouse,
-        role = role      
-    ))
-    
-    connection = engine.connect()
-    
-    dataframe.to_sql(snowflake_table_name, con=engine,index=False,method=pd_writer,if_exists='append')
-    
-    connection.close()
-    engine.dispose()
+    # Close cursor and connection
+    cur.close()
+    con.close()
