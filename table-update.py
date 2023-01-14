ea89caa7168f15514111bd9c12678119460db80f
Rainier Schlekewey <rschlekewey@gmail.com>
2023-01-13 17:22:26 -0800

    Edited to account for data types in table creation

--- table-update.py
+++ table-update.py
@@ -41,49 +41,52 @@ if table name exists in db, table will be appended
 @param desired_table_name is a string of the table name to be created or appended to in snowflake
 @param database string of snowflake database to use
 @param schema string of snowflake schema to use
-
 '''
-def push_snowflake(dataframe, desired_table_name, database, schema):
+def push_snowflake(dataframe, table_name, database=database, schema=schema):
+    
     # Connect to Snowflake
     con = snowflake.connector.connect(
         user=username,
         password=password,
-        account=account
+        account=account,
+        role=role,
+        warehouse=warehouse,
     )
-
-    # Create a cursor
     cur = con.cursor()
-
-    # Snowflake table
-    table_name = desired_table_name
-    table = '.'.join([database, schema, table_name])
-
-    # check for unsupported data types
-    unsupported_types = set(dataframe.dtypes.astype(str).unique()).difference(set(['int','float','bool','object','datetime64[ns]','float64','int64','datetime64[ns, UTC]']))
-    if unsupported_types:
-        raise ValueError(f"Unsupported datatype found: {unsupported_types}. Please make sure that all datatype can be mapped to a Snowflake data type.")
-    # Check if table exists
-    table_exists = cur.execute("SHOW TABLES LIKE '{}'".format(table))
-    if not table_exists:
-        # Create the table with the proper columns and data types
-        columns = ''
-        for col, dtype in zip(dataframe.columns, dataframe.dtypes):
-            columns += col + ' ' + str(dtype) + ','
-        columns = columns[:-1] # remove the last comma
-        cur.execute("CREATE TABLE {}.{} ({})".format(schema, table_name, columns))
-        con.commit()
-
-    cur.execute("USE WAREHOUSE {}".format(warehouse))
-    cur.execute("USE SCHEMA {}".format(schema))
-    cur.execute("USE DATABASE {}".format(database))
     cur.execute("USE ROLE {}".format(role))
+    cur.execute("USE DATABASE {}".format(database))
+    cur.execute("USE SCHEMA {}".format(schema))
 
-    # Insert data into the table
-    data = [tuple(x) for x in dataframe.values]
-    placeholders = ','.join(['%s'] * len(dataframe.columns))
-    cur.executemany("INSERT INTO {} SELECT {}".format(table, placeholders), data)
+    # Check if table already exists
+    cur.execute("SHOW TABLES LIKE '{}'".format(table_name))
+    if not cur.fetchone():
+        # Get column names and data types from dataframe
+        columns = []
+        for column in dataframe.columns:
+            if dataframe[column].dtype == 'object':
+                columns.append("{} VARCHAR(255)".format(column))
+            elif dataframe[column].dtype == 'int64':
+                columns.append("{} INTEGER".format(column))
+            elif dataframe[column].dtype == 'float64':
+                columns.append("{} FLOAT".format(column))
+            elif dataframe[column].dtype == 'datetime64[ns]':
+                columns.append("{} TIMESTAMP".format(column))
+            else:
+                raise ValueError("Data type {} not supported in Snowflake".format(dataframe[column].dtype))
+        columns = ', '.join(columns)
+
+        # Create table
+        cur.execute("CREATE TABLE {}.{} ({})".format(schema, table_name, columns))
+    
+    # Get column names and placeholders for insert statement
+    columns = ', '.join(dataframe.columns)
+    placeholders = ', '.join(['%s' for _ in dataframe.columns])
+
+    # Convert dataframe to list of tuples
+    data = [tuple(x) for x in dataframe.to_numpy()]
+
+    # Insert data into Snowflake table
+    cur.executemany("INSERT INTO {}.{} ({}) VALUES ({})".format(schema, table_name, columns, placeholders), data)
     con.commit()
-
-    # Close cursor and connection
     cur.close()
     con.close()
